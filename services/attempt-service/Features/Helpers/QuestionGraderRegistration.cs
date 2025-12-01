d81e5eff5b1dfdd81223c0e4091d2943ce2e678c
minkhoaa <tukhoa040505@gmail.com>
2025-12-02 00:59:28 +0700

    update: apply factory method for scalability and implementations of other functions

---
+++ services/attempt-service/Features/Helpers/QuestionGraderRegistration.cs
@@ -0,0 +1,107 @@
+using Shared.ExamDto.Contracts.Exam.Enums;
+
+namespace attempt_service.Features.Helpers
+{
+    public interface IQuestionGraderRegistration
+    {
+        // design pattern abstract factory
+        IEnumerable<string> SupportedTypes { get; }
+        IQuestionGrader Create();
+    }
+    public class SingleChoiceGraderRegistration : IQuestionGraderRegistration
+    {
+        private readonly IQuestionGrader _instance;
+        public SingleChoiceGraderRegistration(SingleChoiceGrader instance)
+        {
+            _instance = instance;
+        }
+        public IEnumerable<string> SupportedTypes => new[]
+    {
+        QuestionType.MultipleChoiceSingle,
+        QuestionType.MultipleChoiceSingleImage,
+        QuestionType.TrueFalseNotGiven,
+        QuestionType.YesNoNotGiven,
+    };
+
+        public IQuestionGrader Create() => _instance;
+    }
+    public class CompletionGraderRegistration : IQuestionGraderRegistration
+    {
+        private readonly IQuestionGrader _instance;
+        public CompletionGraderRegistration(CompletionGrader instance)
+        {
+            _instance = instance;
+        }
+        public IEnumerable<string> SupportedTypes =>
+        [
+            QuestionType.SummaryCompletion,
+            QuestionType.TableCompletion,
+            QuestionType.NoteCompletion,
+            QuestionType.FormCompletion,
+            QuestionType.SentenceCompletion
+        ];
+
+        public IQuestionGrader Create() => _instance;
+    }
+    public class LabelGraderRegistration : IQuestionGraderRegistration
+    {
+        private readonly IQuestionGrader _instance;
+        public LabelGraderRegistration(LabelGrader instance)
+        {
+            _instance = instance;
+        }
+        public IEnumerable<string> SupportedTypes => new[]
+     {
+            QuestionType.DiagramLabel,
+            QuestionType.MapLabel,
+        };
+
+        public IQuestionGrader Create() => _instance;
+    }
+    public class MatchingHeadingGraderRegistration : IQuestionGraderRegistration
+    {
+        private readonly IQuestionGrader _instance;
+        public MatchingHeadingGraderRegistration(MatchingHeadingGrader instance)
+        {
+            _instance = instance;
+        }
+        public IEnumerable<string> SupportedTypes => new[]
+     {
+            QuestionType.MatchingEndings,
+            QuestionType.MatchingFeatures,
+            QuestionType.MatchingHeading,
+            QuestionType.MatchingInformation,
+            QuestionType.Classification,
+        };
+
+        public IQuestionGrader Create() => _instance;
+    }
+    public class FlowChartGraderRegistration : IQuestionGraderRegistration
+    {
+        private readonly IQuestionGrader _instance;
+        public FlowChartGraderRegistration(FlowChartGrader instance)
+        {
+            _instance = instance;
+        }
+        public IEnumerable<string> SupportedTypes => new[]
+     {
+            QuestionType.FlowChart
+        };
+
+        public IQuestionGrader Create() => _instance;
+    }
+    public class ShortAnswerGraderRegistration : IQuestionGraderRegistration
+    {
+        private readonly IQuestionGrader _instance;
+        public ShortAnswerGraderRegistration(ShortAnswerGrader instance)
+        {
+            _instance = instance;
+        }
+        public IEnumerable<string> SupportedTypes => new[]
+     {
+            QuestionType.ShortAnswer
+        };
+
+        public IQuestionGrader Create() => _instance;
+    }
+}
