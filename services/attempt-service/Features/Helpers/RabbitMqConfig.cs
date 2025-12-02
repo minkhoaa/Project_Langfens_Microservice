786b75a97d994d0a9c2287a98836cf08e2ce0a15
minkhoaa <tukhoa040505@gmail.com>
2025-12-03 01:53:52 +0700

    add rabbitmq async worker for writing grader

---
+++ services/attempt-service/Features/Helpers/RabbitMqConfig.cs
@@ -0,0 +1,12 @@
+namespace attempt_service.Features.Helpers
+{
+    public record RabbitMqConfig
+    {
+        public string Host { get; init; } = "localhost";
+        public ushort Port { get; init; } = 5672;
+        public string VirtualHost { get; init; } = "/";
+        public string Username { get; init; } = "guest";
+        public string Password { get; init; } = "guest";
+        public bool UseSsl { get; init; } = false;
+    }
+}
