326c55cf92a3bd2c789fadbf5c9fc862f807a88f
minkhoaa <tukhoa040505@gmail.com>
2025-12-07 01:19:18 +0700

    update: synchronize placement, full test result for ielts band, mapping result into placement result

---
+++ services/attempt-service/Migrations/20251206171615_update-placement-test-db.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace attempt_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateplacementtestdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ListeningTotal",
+                table: "placement_result",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReadingTotal",
+                table: "placement_result",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ListeningTotal",
+                table: "placement_result");
+
+            migrationBuilder.DropColumn(
+                name: "ReadingTotal",
+                table: "placement_result");
+        }
+    }
+}
