5370b365218f7ef6d25816a43e25a2472a6eafff
minkhoaa <tukhoa040505@gmail.com>
2025-12-14 00:17:08 +0700

    update: clarify unit test and integration test

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 # Run unit + integration suites sequentially to reduce resource spikes
 
-dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/AttemptService.UnitTests/AttemptService.UnitTests.csproj
-dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/AuthService.UnitTests/AuthService.UnitTests.csproj
-dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/CourseService.UnitTests/CourseService.UnitTests.csproj
-dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/ExamService/ExamService.UnitTest/ExamService.UnitTest.csproj
-dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/VocabularyService.UnitTests/VocabularyService.UnitTests.csproj
-dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/EmailService.UnitTests/EmailService.UnitTests.csproj
-dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/IntegrationTests/IntegrationTests.csproj
+dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/unit/AttemptService.UnitTests/AttemptService.UnitTests.csproj
+dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/unit/AuthService.UnitTests/AuthService.UnitTests.csproj
+dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/unit/CourseService.UnitTests/CourseService.UnitTests.csproj
+dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/unit/ExamService.UnitTest/ExamService.UnitTest.csproj
+dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/unit/VocabularyService.UnitTests/VocabularyService.UnitTests.csproj
+dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/unit/EmailService.UnitTests/EmailService.UnitTests.csproj
+dotnet test /m:1 /p:BuildInParallel=false /p:NodeReuse=false /p:UseSharedCompilation=false "$@" tests/integration/IntegrationTests/IntegrationTests.csproj
