d957fb122c5a8a31a00ad04099ab772e24963dd5
minkhoaa <tukhoa040505@gmail.com>
2025-12-16 00:30:27 +0700

    update: import jsonl

--- services/dictionary-service/Features/Handler/DictionaryHandler.cs
+++ services/dictionary-service/Features/Handler/DictionaryHandler.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using dictionary_service.Features.Service;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace dictionary_service.Features.Handler
 {
@@ -13,6 +14,7 @@ namespace dictionary_service.Features.Handler
         public static Task<IResult> GetSuggestsHandler(string word, string? pos, CancellationToken token, [FromServices] IDictionaryService service) => service.GetSuggests(word, pos, token);
         public static Task<IResult> GetDetailsHandler([FromRoute] int id, CancellationToken token, [FromServices] IDictionaryService service) => service.GetDetails(id, token);
         public static Task<IResult> ReindexHandler(int? batchSize, ElasticIndexer indexer, CancellationToken ct, [FromServices] IDictionaryService service) => service.Reindex(batchSize, indexer, ct);
-        public static Task<IResult> ImportHandler([FromBody] ImportRequest req, CancellationToken ct, [FromServices] IDictionaryService service) => service.Import(req, ct);
+        public static Task<IResult> ImportHandler(IFormFile file, int? maxLines, CancellationToken ct, [FromServices] IDictionaryService service)
+            => service.Import(file, maxLines, ct);
     }
 }
